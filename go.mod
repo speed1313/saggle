b9a1ef392c2af4078bf7d65e758a0a53bb8a495c
speed1313 <speedtry13@icloud.com>
2023-02-02 16:04:54 +0900

    impl search and database

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/speed1313/saggle
 
 go 1.18
 
-require github.com/kljensen/snowball v0.6.0
+require (
+	github.com/kljensen/snowball v0.6.0
+	github.com/mattn/go-sqlite3 v1.14.16
+)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"database/sql"
 	"encoding/xml"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"os"
 	"regexp"
 	"strings"
@@ -118,28 +122,172 @@ func (idx index) add(docs []document) {
 	}
 }
 
+func (idx index) search(text string) []int {
+	var r []int
+	for _, token := range analyze(text) {
+		if ids, ok := idx[token]; ok {
+			if r == nil {
+				r = ids
+			} else {
+				r = intersection(r, ids)
+			}
+		} else {
+			// U \intersection φ = φ
+			return nil
+		}
+	}
+	return r
+}
+
+// a and b is expected to be sorted.
+func intersection(a []int, b []int) []int {
+	maxLen := len(a)
+	if len(b) > maxLen {
+		maxLen = len(b)
+	}
+	r := make([]int, 0, maxLen)
+	var i, j int
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			i++
+		} else if a[i] > b[j] {
+			j++
+		} else {
+			r = append(r, a[i])
+			i++
+			j++
+		}
+	}
+	return r
+}
+
 func main() {
-	//docs, err := loadDocuments("enwiki-latest-abstract1.xml")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//r := search_re(docs, "cat")
-	//for _, doc := range r {
-	//	fmt.Println(doc.Title)
-	//}
-	text := "A donut on a glass plate. Only the donuts."
-	tokens := tokenize(text)
-	fmt.Println(tokens)
-	lower_tokens := lowercaseFilter(tokens)
-	fmt.Println(lower_tokens)
-	stop_word_removed_tokens := stopwordFilter(lower_tokens)
-	fmt.Println(stop_word_removed_tokens)
-	stem_tokens := stemmerFilter(stop_word_removed_tokens)
-	fmt.Println(stem_tokens)
-	analyzed_tokens := analyze(text)
-	fmt.Printf("%v\n", analyzed_tokens)
+	docs, err := loadDocuments("enwiki-latest-abstract1.xml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// if db doesnot exists, create index
 	idx := make(index)
-	idx.add([]document{{ID: 1, Text: "A donut on a glass plate. Only the donuts."}})
-	idx.add([]document{{ID: 2, Text: "donut is a donut"}})
-	fmt.Println(idx)
+	if _, err := os.Stat("./index.db"); os.IsNotExist(err) {
+		idx.add(docs)
+		create_index_db(idx)
+	}
+	for {
+		fmt.Print("Enter your query: ")
+		var s string
+		r := bufio.NewReader(os.Stdin)
+		s, _ = r.ReadString('\n')
+		count := 0
+		for _, id := range search_from_db(s) {
+			fmt.Println(docs[id].Title)
+			count += 1
+
+		}
+		fmt.Printf("Result: %v pages found\n", count)
+	}
+}
+func search_from_db(query string) []int {
+
+	db, err := sql.Open("sqlite3", "./index.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var r []int
+	for _, token := range analyze(query) {
+		rows, err := db.Query("select doc_id from index_table where token = ?", token)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		var ids []int
+		for rows.Next() {
+			var doc_id int
+			err = rows.Scan(&doc_id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			ids = append(ids, doc_id)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if r == nil {
+			r = ids
+		} else {
+			r = intersection(r, ids)
+		}
+	}
+	return r
+}
+
+func create_index_db(idx index) {
+	os.Remove("./index.db")
+	db, err := sql.Open("sqlite3", "./index.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table index_table (token text not null, doc_id integer);
+	delete from index_table;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("insert into index_table(token, doc_id) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	for token, doc_ids := range idx {
+		for _, doc_id := range doc_ids {
+			_, err = stmt.Exec(token, doc_id)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("select token, doc_id from index_table")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var token string
+		var doc_id int
+		err = rows.Scan(&token, &doc_id)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err = db.Prepare("select doc_id from index_table where token=?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var doc_id []int
+	err = stmt.QueryRow("cat").Scan(&doc_id)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
